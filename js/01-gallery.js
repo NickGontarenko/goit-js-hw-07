ed85232fc712aa4095671a210bec558e139cc38c
NickGontarenko <nick.gontarenko@gmail.com>
2022-01-28 00:11:19 +0200

    markup gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('div.gallery');
+const galleryMarkupRef = createGalleruMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkupRef);
+
+function createGalleruMarkup(items) {
+    return items
+        .map(item => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+</div>`;
+        })
+        .join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('ul.gallery');
+const markupGalleruRef = createMarkupGallery(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML('beforeend', markupGalleruRef);
+
+function createMarkupGallery(elements) {
+    return elements
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+        })
+        .join('');
+}
